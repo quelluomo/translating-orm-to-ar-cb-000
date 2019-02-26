4b8aaab6069ed8ba5cdb53e1f995264bbb62789b
Matteo Muscas <thatman.j@gmail.com>
2019-02-26 15:42:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,4 @@
 class Dog < ActiveRecord::Base
-  sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS dogs (
-  id INTEGER PRIMARY KEY,
-  name TEXT
-  )
-SQL
- ActiveRecord::Base.connection.execute(sql)
- end
+
 
 end
